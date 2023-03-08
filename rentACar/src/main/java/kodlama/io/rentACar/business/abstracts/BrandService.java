0e1784e80511b1da7324a7fda6698b641e575127
Furkanaricc <furkannyanak@outlook.com>
2023-03-08 10:10:46 +0300

    Initial commit

---
+++ rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -0,0 +1,7 @@
+package kodlama.io.rentACar.business.abstracts;
+
+import kodlama.io.rentACar.entitiy.concretes.Brand;
+import java.util.*;
+public interface BrandService {
+    List<Brand> getAll();
+}
---
+++ rentACar/src/main/java/kodlama/io/rentACar/entitiy/concretes/Brand.java
@@ -0,0 +1,24 @@
+package kodlama.io.rentACar.entitiy.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name="brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="name")
+    private String name;
+}
